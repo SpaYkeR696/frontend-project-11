e6d4bc016584408a27935044901d75484820976d
SpaYkeR669 <spayker666999@gmail.com>
2023-11-03 12:05:30 +0300

    add locales ru

---
+++ src/locales/ru.js
@@ -0,0 +1,18 @@
+export default {
+  traslation: {
+    header1: 'RSS агрегатор',
+    header2: 'Начните читать RSS сегодня! Это легко, и красиво.',
+    urlExample: 'Пример: https://rss.nytimes.com/services/xml/rss/nyt/World.xml',
+    btnSubmit: 'Добавить',
+    inputLabel: 'Ссылка RSS',
+    succesMessage: 'RSS успешно загружен',
+    existingRssError: 'RSS уже существует',
+    invalidRssForm: 'Ссылка должна быть валидным URL',
+    emptyRss: 'Не должно быть пустым',
+    noRssError: 'Ресурс не содержит валидный RSS',
+    'Network Error': 'Ошибка сети',
+    read: 'Читать полностью',
+    close: 'Закрыть',
+    see: 'Просмотр',
+  },
+};
