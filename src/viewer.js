10ebdc3a97798041cc3c8bcaf718d33f916f2822
SpaYkeR669 <spayker666999@gmail.com>
2023-12-08 17:15:45 +0300

    fex function

--- src/viewer.js
+++ src/viewer.js
@@ -1,139 +1,143 @@
 const createWrapper = (heading, items, i18next) => {
-  const divElement = document.createElement('div');
-  divElement.classList.add('card', 'border-0');
+  const divElement = document.createElement("div");
+  divElement.classList.add("card", "border-0");
 
-  const cardBodyElement = document.createElement('div');
-  cardBodyElement.classList.add('card-body');
+  const cardBodyElement = document.createElement("div");
+  cardBodyElement.classList.add("card-body");
 
-  const h2Element = document.createElement('h2');
-  h2Element.classList.add('card-title', 'h4');
-  h2Element.innerText = i18next.t(heading);
+  const h2Element = document.createElement("h2");
+  h2Element.classList.add("card-title", "h4");
+  h2Element.textContent = i18next.t(heading);
 
   cardBodyElement.appendChild(h2Element);
   divElement.appendChild(cardBodyElement);
 
-  const ulElement = document.createElement('ul');
-  ulElement.classList.add('list-group', 'border-0', 'rounded-0');
-  items.forEach((item) => {
-    const liElement = document.createElement('li');
-    liElement.classList.add('list-group-item');
-    liElement.innerText = item;
-    ulElement.appendChild(liElement);
-  });
+  const ulElement = document.createElement("ul");
+  ulElement.classList.add("list-group", "border-0", "rounded-0");
+  items.forEach((item) => ulElement.insertAdjacentHTML("beforeend", item));
 
   divElement.appendChild(ulElement);
   return divElement.outerHTML;
 };
 
 const renderContent = (elements, state, i18next) => {
-  elements.postContainer.innerHTML = '';
-  elements.feedContainer.innerHTML = '';
+  elements.postContainer.innerHTML = "";
+  elements.feedContainer.innerHTML = "";
 
   const posts = state.content.posts.map(({ postTitle, postLink, postId }) => {
-    const linkElement = document.createElement('a');
-    const buttonElement = document.createElement('button');
+    const linkElement = document.createElement("a");
+    const buttonElement = document.createElement("button");
 
-    const linkClass = state.ui.posts.has(postId) ? 'fw-normal' : 'fw-bold';
+    const linkClass = state.ui.posts.has(postId) ? "fw-normal" : "fw-bold";
 
     linkElement.href = postLink;
-    linkElement.target = '_blank';
-    linkElement.rel = 'noopener noreferrer';
+    linkElement.target = "_blank";
+    linkElement.rel = "noopener noreferrer";
     linkElement.dataset.id = postId;
     linkElement.dataset.link = true;
     linkElement.classList.add(linkClass);
-    linkElement.innerText = postTitle;
+    linkElement.textContent = postTitle;
 
-    buttonElement.type = 'button';
-    buttonElement.classList.add('btn', 'btn-outline-primary', 'btn-sm');
+    buttonElement.type = "button";
+    buttonElement.classList.add("btn", "btn-outline-primary", "btn-sm");
     buttonElement.dataset.id = postId;
-    buttonElement.dataset.bsToggle = 'modal';
-    buttonElement.dataset.bsTarget = '#modal';
-    buttonElement.innerText = i18next.t('view');
+    buttonElement.dataset.bsToggle = "modal";
+    buttonElement.dataset.bsTarget = "#modal";
+    buttonElement.textContent = i18next.t("view");
 
-    const liElement = document.createElement('li');
-    liElement.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-start', 'border-0', 'border-end-0');
+    const liElement = document.createElement("li");
+    liElement.classList.add(
+      "list-group-item",
+      "d-flex",
+      "justify-content-between",
+      "align-items-start",
+      "border-0",
+      "border-end-0"
+    );
     liElement.appendChild(linkElement);
     liElement.appendChild(buttonElement);
 
     return liElement.outerHTML;
   });
 
-  const postWrapper = createWrapper('posts', posts, i18next);
-  elements.postContainer.insertAdjacentHTML('afterbegin', postWrapper);
+  const postWrapper = createWrapper("posts", posts, i18next);
+  elements.postContainer.insertAdjacentHTML("afterbegin", postWrapper);
 
   const feeds = state.content.feeds.map(({ feedTitle, feedDescription }) => {
-    const h3Element = document.createElement('h3');
-    const pElement = document.createElement('p');
+    const h3Element = document.createElement("h3");
+    const pElement = document.createElement("p");
 
-    h3Element.classList.add('h6', 'm-0');
-    h3Element.innerText = feedTitle;
+    h3Element.classList.add("h6", "m-0");
+    h3Element.textContent = feedTitle;
 
-    pElement.classList.add('m-0', 'small');
-    pElement.innerText = feedDescription;
+    pElement.classList.add("m-0", "small");
+    pElement.textContent = feedDescription;
 
-    const liElement = document.createElement('li');
-    liElement.classList.add('list-group-item', 'border-0', 'border-end-0');
+    const liElement = document.createElement("li");
+    liElement.classList.add("list-group-item", "border-0", "border-end-0");
     liElement.appendChild(h3Element);
     liElement.appendChild(pElement);
 
     return liElement.outerHTML;
   });
 
-  const feedWrapper = createWrapper('feeds', feeds, i18next);
-  elements.feedContainer.insertAdjacentHTML('afterbegin', feedWrapper);
+  const feedWrapper = createWrapper("feeds", feeds, i18next);
+  elements.feedContainer.insertAdjacentHTML("afterbegin", feedWrapper);
 };
 
 const handleErrors = (elements, state, i18next) => {
-  if (elements.feedback.classList.contains('text-success')) {
-    elements.feedback.classList.add('text-danger');
-    elements.feedback.classList.remove('text-success');
+  if (elements.feedback.classList.contains("text-success")) {
+    elements.feedback.classList.add("text-danger");
+    elements.feedback.classList.remove("text-success");
   }
 
   if (Object.keys(state.form.errors).length !== 0) {
     elements.feedback.textContent = i18next.t(state.form.errors.url);
-    elements.urlInput.classList.add('is-invalid');
+    elements.urlInput.classList.add("is-invalid");
   } else {
-    elements.feedback.textContent = '';
-    elements.urlInput.classList.remove('is-invalid');
+    elements.feedback.textContent = "";
+    elements.urlInput.classList.remove("is-invalid");
   }
 };
 
 const handleSuccess = (elements, i18next) => {
-  if (elements.feedback.classList.contains('text-danger')) {
-    elements.feedback.classList.remove('text-danger');
-    elements.feedback.classList.add('text-success');
-    elements.feedback.textContent = i18next.t('success');
+  if (elements.feedback.classList.contains("text-danger")) {
+    elements.feedback.classList.remove("text-danger");
+    elements.feedback.classList.add("text-success");
+    elements.feedback.textContent = i18next.t("success");
   }
 };
 
 const changeUiAnchors = (elements, applyData) => {
   const id = applyData.args[0];
-  const anchor = elements.postContainer.querySelector(`[data-link][data-id="${id}"]`);
+  const anchor = elements.postContainer.querySelector(
+    `[data-link][data-id="${id}"]`
+  );
 
-  anchor.classList.remove('fw-bold');
-  anchor.classList.add('fw-normal');
+  anchor.classList.remove("fw-bold");
+  anchor.classList.add("fw-normal");
 };
 
 const handleStatus = (elements, value, i18next) => {
   switch (value) {
-    case 'fetching':
+    case "fetching":
       elements.urlInput.disabled = true;
       elements.submit.disabled = true;
       break;
-    
-    case 'error':
+
+    case "error":
       elements.urlInput.disabled = false;
       elements.submit.disabled = false;
       break;
-    
-    case 'success':
+
+    case "success":
       handleSuccess(elements, i18next);
       elements.urlInput.disabled = false;
       elements.submit.disabled = false;
       elements.form.reset();
       elements.urlInput.focus();
       break;
-      
+
     default:
       throw new Error(`unknown process ${value}`);
   }
@@ -141,24 +145,24 @@ const handleStatus = (elements, value, i18next) => {
 
 const watch = (path, value, applyData, elements, state, i18next) => {
   switch (path) {
-    case 'form.errors':
+    case "form.errors":
       handleErrors(elements, state, i18next);
       break;
-    
-    case 'form.status':
+
+    case "form.status":
       handleStatus(elements, value, i18next);
       break;
-     
-    case 'content.posts':
+
+    case "content.posts":
       renderContent(elements, state, i18next);
       break;
-      
-    case 'ui.posts':
+
+    case "ui.posts":
       changeUiAnchors(elements, applyData);
       break;
-      
+
     default:
-      break;  
+      break;
   }
 };
 
